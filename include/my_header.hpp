5323e5315d47ae51bfab0f8dcbf386c2bd8f678c
eleve <eleve@si-PC08>
2018-01-22 18:18:06 +0100

    j'ai preque fini l'exo3

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -4,6 +4,9 @@
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
+
+#include <cmath>
+
 double exo1(double rayon)
 {
 	double v;
@@ -17,5 +20,11 @@ void exo2(short a, short b)
 	y = a<b&&!a;
 }
 
+double exo3(double rayon)
+{
+	double aire;
+	aire = 3.14 *pow(rayon, 02f);
+	return aire;
+}
 #endif // MY_HEADER_HPP
 
