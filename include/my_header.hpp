3b7d4aff7a6d76f048250ab6e3abf06c3de6ac8b
eleve <eleve@si-PC08>
2018-01-22 17:56:41 +0100

    j'ai fini exo1

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -1,9 +1,15 @@
 //  Copyright (c) 2017 Antoine Tran Tan
+//  Copyright (c) 2017 Colin Chouraki
 //
 
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
-
+double exo1(double rayon)
+{
+	double v;
+	v = 4/3 *3.14 * rayon * rayon * rayon;
+	return v;
+}
 
 
 #endif // MY_HEADER_HPP
